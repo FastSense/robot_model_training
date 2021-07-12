97e200ab893396ccb75453dcb506919982e273b1
Ruslan <hocop@yandex.ru>
2021-07-12 19:02:59 +0300

    changed docker

--- docker/run.sh
+++ docker/run.sh
@@ -8,10 +8,8 @@ docker run -it -d --privileged --net=host \
       --runtime=nvidia \
       --gpus=all \
       -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-      -v ${PWD}/../:/home/user/catkin_ws/src:rw \
+      -v ${PWD}/../:/home/user/ws:rw \
       -e DISPLAY=$DISPLAY \
       -e NVIDIA_VISIBLE_DEVICES="all" \
       -e NVIDIA_DRIVER_CAPABILITIES="all" \
-      -e ROS_HOSTNAME="localhost" \
-      -e ROS_MASTER_URI="http://localhost:11311" \
       -e QT_X11_NO_MITSHM=1 $image_name zsh 
